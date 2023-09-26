4fe0ef4d0eb529258eff85e3d96476cf74316488
VXZK9944 <catalepsiaprojetiva@gmail.com>
2023-09-26 15:09:11 -0300

    prova
    Aline e Ronaldo

---
+++ prova.py
@@ -0,0 +1,10 @@
+num1 = int ( input ("digite uma nota"))
+num2 = int ( input ("digite nota dois"))
+num3 = int ( input ("digite nota tres"))
+num4 = int ( input ("digite nota quatro"))
+num5 = int ( input( "digite nota cinco"))
+# calculo de soma
+res = num1+num2+num3+num4+num5
+res = res/5
+
+print('a media enttre as notas{},{},{},{} e {} e {}'.format(num1,num2,num3,num4,num5))
